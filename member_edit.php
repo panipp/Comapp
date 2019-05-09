493d31e1dc892e119d0fdaafdc2614afac86d1e1
bossbaewjai <boss_frozen@hotmail.com>
2019-05-09 17:02:00 +0700

    Update member_edit.php

--- member_edit.php
+++ member_edit.php
@@ -3,6 +3,7 @@
 
 		if (!$_SESSION['hr_loggedin']){
 			echo "<script language='javascript'> alert('กรุณาเข้าสู่ระบบก่อน');window.location='login.php';</script>";
+      exit;
 	}
 
 	$empID = $_GET['employeeID'];
@@ -81,7 +82,7 @@
                     <li><a href="about_hr.php">About</a></li>
                     <li><a href="news_hr.php">News</a></li>
                     <li><a href="search_hr.php">Search</a></li>
-					<li><a href="login.php">Logout</a></li>
+					          <li><a href="logout.php">Logout</a></li>
                 </ul>
             </nav>
 
