3b4020f4205fece50bd143d6fd1deaa6a1a89a9f
bossbaewjai <boss_frozen@hotmail.com>
2019-05-09 17:02:09 +0700

    Update search_hrEdit.php

--- search_hrEdit.php
+++ search_hrEdit.php
@@ -3,6 +3,7 @@
 
 	if (!$_SESSION['hr_loggedin']){
 		echo "<script language='javascript'> alert('กรุณาเข้าสู่ระบบก่อน');window.location='login.php';</script>";
+		exit;
 }
 	$_SESSION['staff'] = true;
 	$searching = preg_replace("#[^0-9a-z]#i","",$_GET['input']);
@@ -27,7 +28,7 @@
 				$_SESSION['staff'] = true;
 			}
 		}
-		
+
 	}
 
 	if(isset($_POST['delete'])){
@@ -63,7 +64,7 @@
                     <li><a href="about_hr.php">About</a></li>
                     <li><a href="news_hr.php">News</a></li>
                     <li><a href="search_hr.php">Search</a></li>
-					<li><a href="login.php">Logout</a></li>
+										<li><a href="logout.php">Logout</a></li>
                 </ul>
             </nav>
         </aside>
